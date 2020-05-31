df0446b46126e9ad972f8aefc14bfbd289010619
Kisha Codes <Kishacodes@gmail.com>
2020-05-31 12:16:44 -0400

    2. Parts 5 of 8 done. Stuck on shouldShowWarning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Satellite } from "./satellite";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  sourceList: Satellite[];
   constructor() {
     // title = "orbit-report";
     // headers: ["Name", "Type", "Operational", "Orbit Type", "Launch Date"];
@@ -18,27 +19,27 @@ export class AppComponent {
       function (response) {
         response.json().then(
           function (data) {
-            let fetchedSatellites = data.satellites;
-            let satellite;
+            let fetchedSatellites: Satellite[] = data.satellites;
+            let satellite: object;
             // TODO: loop over satellites
             for (let i = 0; i < fetchedSatellites.length; i++) {
               satellite = new Satellite(
-                fetchedSatellites[i].name,
-                fetchedSatellites[i].type,
-                fetchedSatellites[i].launchDate,
-                fetchedSatellites[i].orbitType,
-                fetchedSatellites[i].operational
+                fetchedSatellites[i]["name"],
+                fetchedSatellites[i]["type"],
+                fetchedSatellites[i]["launchDate"],
+                fetchedSatellites[i]["orbitType"],
+                fetchedSatellites[i]["operational"]
               );
               this.sourceList.push(satellite);
             }
-            // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-
-            // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
           }.bind(this)
         );
       }.bind(this)
     );
   }
+  // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+
+  // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
   // title = "orbit-report";
   // headers: ["Name", "Type", "Operational", "Orbit Type", "Launch Date"];
   // sourceList: Satellite[];
